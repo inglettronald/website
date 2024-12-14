1469fdfeca01904758418cd413217fce82123bf6
Ronald Inglett <71849533+inglettronald@users.noreply.github.com>
2024-12-14 13:13:18 -0500

    Add a preact usage and update pnpm-lock.yaml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://dulkir.com"
+  site: "https://dulkir.com",
+  integrations: [preact()]
 });
